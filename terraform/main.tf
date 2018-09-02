cba6657306a094e3ffe67b300d128ab43c12fba5
Mike Lazzaro <mike@lazza.ro>
2018-09-02 15:54:17 -0400

    Bastion instance should be using vpc_security_group_ids, not security_groups (which forces terraform to recreate a bunch of stuff each time)

--- terraform/main.tf
+++ terraform/main.tf
@@ -79,7 +79,8 @@ resource "aws_instance" "bastion" {
   key_name = "${var.human_keypair_name}"
 //  vpc_security_group_ids  = ["aws_security_group.demo-bastion-sg.id"]
 //  security_groups = ["aws_security_group.demo-bastion-sg.id"]
-  security_groups = ["${aws_security_group.demo-bastion-sg.id}"]
+//  security_groups = ["${aws_security_group.demo-bastion-sg.id}"]
+  vpc_security_group_ids  = ["${aws_security_group.demo-bastion-sg.id}"]
 
   # Don't need to specify VPC, since subnet will take care of that
 //  subnet_id = "${aws_subnet.public_subnet.id}"
