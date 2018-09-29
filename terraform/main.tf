150af917dd1e9f3391403752be3eabff24ad87c8
Mike Lazzaro <mike@lazza.ro>
2018-09-29 17:47:57 -0400

    Added remaining setup of Python3-based Ansible to bastion host setup

--- terraform/main.tf
+++ terraform/main.tf
@@ -148,7 +148,8 @@ resource "aws_instance" "bastion" {
       "sudo apt-get update",
 //      "sudo apt-get install python-pip -y",
 //      "pip install ansible",
-//      "pip3 install ansible",
+      "sudo apt-get install python3-pip libffi-dev libssl-dev -y"
+      "pip3 install ansible",
       "git clone https://github.com/mikelazzaro/ansible-dns.git",
       "mkdir -p /home/ubuntu/.ssh"
     ]
